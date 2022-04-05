067dbaca08080ba4f61f650f4598b448f5342ab8
jazibghori <77227927+jazibghori@users.noreply.github.com>
2022-04-05 11:55:45 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,12 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showArrow(ArrowNames.North)
+    basic.pause(2000)
+    basic.showArrow(ArrowNames.East)
+    basic.pause(2000)
+    basic.showArrow(ArrowNames.South)
+    basic.pause(2000)
+    basic.showArrow(ArrowNames.West)
+})
+basic.forever(function () {
+	
+})
